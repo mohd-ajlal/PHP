88cc41fa4384efc7c2e28340a2627692bbec3689
Mohd Ajlal <ajlal00786@gmail.com>
2023-04-09 14:27:18 +0530

    PHP

---
+++ Basic/echo.php
@@ -0,0 +1,10 @@
+<?php  
+    echo "Hello World". "<br>"; 
+    echo ("Hello World"). "<br>";
+    echo 'Hello World'. "<br>";
+    echo "Hello World", "Hello World". "<br>";
+    echo "Hello World" . "Hello World". "<br>";
+    echo 5412.25. "<br>";
+    echo "<h1>Hello World</h1>";
+
+?>
